55924916271aa916d4b092b0d7490d86edcf50f5
vicktor349 <ayomidevictor349@gmail.com>
2023-11-23 12:59:19 +0100

    First commit

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+namespace library.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int DisplayOrder { get; set; }
+    }
+}
